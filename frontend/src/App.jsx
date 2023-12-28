9032162c53b31ff76b3cdf8ab727d70588ef2a7e
KangSaid <sasamuhammadsaid.02@gmail.com>
2023-12-28 11:23:35 +0700

    add navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,21 @@
-
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import NavbarComponent from './components/NavbarComponent';
+import PokemonList from './pages/PokemonList';
+import MyPokemon from './pages/MyPokemon';
+import PokemonDetail from './pages/PokemonDetail';
 
 function App() {
-  return <></>
+  return (
+    <>
+      <NavbarComponent />
+      <Routes>
+          <Route path='/' element={<PokemonList />} />
+          <Route path='/mypokemon' element={<MyPokemon />} />
+          <Route path='/pokemondetail/:pokemonName' element={<PokemonDetail />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;

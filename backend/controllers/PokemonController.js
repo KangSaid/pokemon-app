c080a9024edb865357e45fe6008ec82bb27b5288
KangSaid <sasamuhammadsaid.02@gmail.com>
2023-12-30 05:43:22 +0700

    create api get my pokemon by limit & offset

--- backend/controllers/PokemonController.js
+++ backend/controllers/PokemonController.js
@@ -6,22 +6,77 @@ export const createPokemon = async (req, res) => {
     const { pokemon_id, nickname } = req.body;
 
     try {
-        
+
         //probalitiy 50% 
         const randomInteger = Math.floor((Math.random() * 10) + 1);
         const probality = randomInteger <= 5 ? true : false;
 
         const pokemon = probality && (await prisma.pokemon.create({
-            data : {
+            data: {
                 pokemon_id: pokemon_id,
                 nickname: nickname,
             }
-        })); 
+        }));
 
-        const result = !pokemon ? { probality: probality, msg: "Oops, you can't catching this pokemon" } : {...pokemon, probality: true} 
+        const result = !pokemon ? { probality: probality, msg: "Oops, you fail catching this pokemon, please try again." } : { ...pokemon, probality: true }
 
         res.status(201).json(result)
     } catch (error) {
-        res.status(500).json({error : error})
+        res.status(404).json({ msg: error.message })
     }
+}
+
+export const getPokemon = async (req, res) => {
+    try {
+
+        const pokemonCount = await prisma.pokemon.count()
+        let result = { msg: 'no data' };
+
+        if (pokemonCount != 0) {
+
+            if (req.query.limit) return getPokemonPage(req, res, pokemonCount)
+            
+            const response = await prisma.pokemon.findMany({
+                take: 2,
+                orderBy: {
+                    id: 'asc'
+                }
+            })
+
+            result = {
+                count: pokemonCount,
+                results: response
+            }
+        }
+
+
+        res.status(200).json(result)
+    } catch (error) {
+        res.status(404).json({ msg: error.message })
+    }
+}
+
+const getPokemonPage = async (req, res, count) => {
+
+    try {
+
+        const offset = parseInt(req.query.offset)
+        const limit = parseInt(req.query.limit)
+
+        const response = await prisma.pokemon.findMany({
+            skip: offset,
+            take: limit,
+            orderBy: {
+                id: 'asc'
+            }
+        })
+
+        const result = { count: count, results: response }
+
+        res.status(200).json(result)
+
+    } catch (error) {
+        res.status(404).json({ msg: error.message })
+    }
+
 }
--- backend/routes/PokemonRoute.js
+++ backend/routes/PokemonRoute.js
@@ -1,10 +1,12 @@
 import express from "express"
 import {
-    createPokemon 
+    createPokemon,
+    getPokemon
 } from '../controllers/PokemonController.js';
 
 const router = express.Router();
 
 router.post('/pokemons', createPokemon);
+router.get('/pokemons', getPokemon)
 
 export default router;

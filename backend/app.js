b209a0409130828ff1a3cf39efc065c9849244d6
KangSaid <sasamuhammadsaid.02@gmail.com>
2023-12-29 15:28:11 +0700

    env config

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,15 @@
 import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 
-app.listen(3000, () => {
+app.use(cors())
+app.use(express.json())
+
+
+app.listen(process.env.APP_PORT, () => {
     console.log('testing')
 })
